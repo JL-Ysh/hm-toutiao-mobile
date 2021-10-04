be88f7afaf033bb7294bde78631b8431e0950c29
Ysh <19888812028@139.com>
2021-10-04 09:56:46 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
+import './assets/styles/index.less'
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
